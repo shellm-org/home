614388c3ac7e7da6fa89b0c9da1b527e54b34125
Timothée Mazzucotelli <timothee.mazzucotelli@protonmail.com>
2018-07-27 19:03:48 +0200

    Fix package.sh

--- package.sh
+++ package.sh
@@ -1,5 +1,4 @@
-BINS="bin/home"
-LIBS="lib/home.sh"
+BINS="bin/home-data:bin/home-init:bin/home-new:bin/home-run"
 DEPS=gitlab.com/shellm/shellman
 BASH_COMPLETIONS="cmp/home.completion.bash"
 ZSH_COMPLETIONS="cmp/home.completion.zsh"
